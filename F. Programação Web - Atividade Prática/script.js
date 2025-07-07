885916d8f77dfdab485a7821fff129de4a830da1
Lucas Bicudo <125294423+l-bicudo@users.noreply.github.com>
2025-07-07 18:11:25 -0300

    Add files via upload

--- F. Programação Web - Atividade Prática/script.js
+++ F. Programação Web - Atividade Prática/script.js
@@ -1,7 +1,7 @@
 const links = document.querySelectorAll(".navbar-links a");
 const sections = document.querySelectorAll("section");
 
-// Update active link when clicked
+// Acrescenta efeito hover ao clicar no link na navbar
 links.forEach(link => {
   link.addEventListener("click", () => {
     links.forEach(l => l.classList.remove("ativo"));
@@ -9,22 +9,12 @@ links.forEach(link => {
   });
 });
 
-// Update active link on scroll
-window.addEventListener("scroll", () => {
-  let scrollPos = window.scrollY + 100;
+// Confirmação de envio de formulário com popup
+const form = document.querySelector("form");
 
-  sections.forEach(section => {
-    const top = section.offsetTop;
-    const height = section.offsetHeight;
-    const id = section.getAttribute("id");
-
-    if (scrollPos >= top && scrollPos < top + height) {
-      links.forEach(link => {
-        link.classList.remove("ativo");
-        if (link.getAttribute("href") === `#${id}`) {
-          link.classList.add("ativo");
-        }
-      });
-    }
-  });
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Formulário enviado com sucesso!");
+  // Limpa o formulário após envio
+  form.reset(); 
 });
